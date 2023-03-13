458ea36b03df0fb5eca74cf9806e0af8c419f660
AndrewBavyka <abavyka@list.ru>
2023-03-13 15:01:19 +0500

    готов блок преимущества

---
+++ components/advanatages/advanatages.php
@@ -0,0 +1,41 @@
+<section class="advantages">
+    <div class="advantages-title">
+        <h2 class="section-title">Быстро запускается</h2>
+        <p class="section-description">3-5 дней - это на порядок быстрее
+            по сравнению <br> с запуском аналогичного функционала с нуля.
+            Окупаемость <br> - один месяц!
+        </p>
+    </div>
+
+    <div class="advantages-card">
+        <div class="card-item">
+            <div class="card-item__content">
+                <img src="images/advantages/union-icon.svg" alt="card-icon">
+                <h4 class="card-title">Дизайн</h4>
+                <p class="card-description">
+                    Подберите подходящую цветовую гамму, <br>
+                    замените логотип</p>
+            </div>
+        </div>
+        <div class="card-item">
+            <div class="card-item__content">
+                <img src="images/advantages/info-icon.svg" alt="card-icon">
+                <h4 class="card-title">Информация</h4>
+                <p class="card-description">
+                    Заполните информацию о компании и расскажите про свои преимущества</p>
+            </div>
+        </div>
+        <div class="card-item">
+            <div class="card-item__content">
+                <img src="images/advantages/content-icon.svg" alt="card-icon">
+                <h4 class="card-title">Контент</h4>
+                <p class="card-description">
+                    Загрузите каталог товаров
+                    или список услуг <br> и сайт готов</p>
+            </div>
+        </div>
+    </div>
+    <div class="more-info">
+        <a href="#" class="primary-button">Пошаговый план запуска</a>
+    </div>
+</section>
---
+++ components/header.php
@@ -0,0 +1,13 @@
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Allcorp</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+         
+        </header>
