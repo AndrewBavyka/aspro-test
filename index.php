bd52cd115fe41c4ec202fc3fd8577f17ddb978d4
AndrewBavyka <abavyka@list.ru>
2023-03-13 15:13:43 +0500

    готов блок преимущества

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php require_once __DIR__ . '/components/header.php' ?>
 <?php require_once __DIR__ . '/components/advanatages/advanatages.php'; ?>
+<?php require_once __DIR__ . '/components/form/form.php'; ?>
 <?php require_once __DIR__ . '/components/footer.php'; ?>
