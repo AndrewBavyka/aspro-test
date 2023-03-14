9cfe911c0b4aedbc52bb102a70cd92491da2b1f4
AndrewBavyka <abavyka@list.ru>
2023-03-14 16:25:38 +0500

    готова форма

--- components/form/form.php
+++ components/form/form.php
@@ -1,3 +1,27 @@
-<section class="form">
-   
+<section class="section-form">
+    <div class="form-title">
+        <p class="section-name">Контакты</p>
+        <h2 class="section-title">Свяжитесь с нами</h2>
+        <p class="section-description">ООО "Компания" — учрежденное одним или несколькими юридическими или физическими лицами хозяйственное <br> общество. Участники общества не отвечают по его обязательствам </p>
+    </div>
+    <form action="send">
+        <div class="form-container">
+            <div class="form-item name">
+                <label class="input-title">Ваше имя</label>
+                <input class="input-name" type="text">
+            </div>
+            <div class="form-item phone">
+                <label class="input-title">Ваш телефон</label>
+                <input class="input-phone" type="tel">
+            </div>
+            <div class="form-item question">
+                <label class="input-title">Ваш вопрос</label>
+                <textarea name="question" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+        <div class="consent-container">
+            <label class="consent">Я согласен на обработку <a href="#">персональных данных</a></label>
+        </div>
+        <button class="primary-button send-form" type="submit">Регистрация</button>
+    </form>
 </section>
