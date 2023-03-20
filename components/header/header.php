42800385a53d40ed7239b566e7a21c14f794c695
AndrewBavyka <abavyka@list.ru>
2023-03-21 00:24:48 +0500

    начата шапка

---
+++ components/header/header.php
@@ -0,0 +1,52 @@
+<header class="header">
+    <div class="header__top">
+        <div class="regionality">
+            <img src="" alt="regionality-icon">
+            <div class="regionality-cities">
+                <p>Челябинск</p>
+                <img src="" alt="arrow-icon">
+            </div>
+        </div>
+        <div class="business-address">
+            <img src="" alt="location-icon">
+            <address>Челябинск, ул. Лермонтова 21, 3 этаж, офис 4</address>
+        </div>
+        <div class="social-media">
+            <a href="#">
+                <img src="" alt="social-icon">
+            </a>
+            <a href="#">
+                <img src="" alt="social-icon">
+            </a>
+            <a href="#">
+                <img src="" alt="social-icon">
+            </a>
+            <a href="#">
+                <img src="" alt="social-icon">
+            </a>
+        </div>
+        <div class="request-call">
+            <div class="phone">
+                <img src="" alt="phone-icon">
+                <a href="tel:+79023456789" class="request-call__phone">+7(902)-345-67-89 </a>
+            </div>
+            <a href="#" class="request-call__link">Заказать звонок</a>
+        </div>
+        <div class="line-block">
+            <div class="language-selection">
+                <img src="" alt="">
+                <div class="language-selection__dropdown">
+                    RU
+                    <img src="#" alt="arrow-icon">
+                </div>
+            </div>
+            <a href="#" class="profile">
+                <img src="" alt="">
+            </a>
+            <a href="#" class="сart">
+                <img src="" alt="cart-icon">
+            </a>
+        </div>
+    </div>
+    <div class="header__bottom"></div>
+</header>
